c6223d1424fbb09c98af87ede3a63d0a04e7a96d
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-09-25 17:45:52 +0530

    i create function component and render both

--- App.js
+++ App.js
@@ -11,25 +11,42 @@ console.log(heading);
 
 // JXS (transpiled before it reaches the js) - parcel - Babel
       
-// React Elament 
-const Jsxheading = () => (
-    <h1 className="head" tabIndex="5">
-    This is  JSX and using Babal 
-    </h1>
-)
+          //  React Elament 
+
+// const Jsxheading = () => (
+//     <h1 className="head" tabIndex="5">
+//     This is  JSX and using Babal 
+//     </h1>
+// )
 
 
-// React Functional component
+         //  React Functional component
+         // component Composition
 
-const FunctionalComponent = () => (
-    <div id="container">
-        <Jsxheading/>
-        <h1 className="heading">Hello IT'S Arshad HERE...</h1>
+// const FunctionalComponent = () => (
+//     <div id="container">
+//         <Jsxheading/>
+//         <h1 className="heading">Hello IT'S Arshad HERE...</h1>
+//     </div>
+// )
+
+const Fact = () => (
+    <div>
+        <Fact1/>
+        <h1>Hello World</h1>
     </div>
 )
 
-console.log(Jsxheading);
+
+const Fact1 = () =>{
+   
+    return <h1> Hello</h1>
+}
+
+// console.log(Jsxheading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<FunctionalComponent/>)
+root.render(<Fact/>)
+
+// root.render(<FunctionalComponent/>)
