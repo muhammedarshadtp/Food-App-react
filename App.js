eb0d17f15f65d7b71ec5d7f57f031929d0765763
Muhammed Arshad T P <arshadtp89@gmail.com>
2024-09-27 16:04:09 +0530

    display header and logo and navbar

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
-
+import logo from "../React/public/image/logo.jpg"
 /**
  * Header
  * -Logo
@@ -15,7 +15,44 @@ import ReactDOM from "react-dom/client";
  * -Contact
  */
 
+
+const Header = () =>{
+    return(
+        <div className="header">
+          <div className="logo-container">
+            <img 
+            className="logo" 
+            src={logo}/>
+            </div>  
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+
+            </div>
+
+        </div>
+    )
+}
+
+  const AppLayout = ()=>{
+    return (
+        <div className="app">
+            <Header/>
+        </div>
+    )
+  }
+
+
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<Fact/>)
+root.render(<AppLayout/>)
 
