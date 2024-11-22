94d6e9a3810a599a7fc55c438ec586793d0d9f44
muhammedarshadtp <arshadtp89@gmail.com>
2024-11-22 12:16:49 +0530

    i am using context in class base components also

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import UserClass from "./UserClass"
+import userContext from "../utils/userContext"
 
 class About extends React.Component{
   constructor(props){
@@ -13,7 +14,22 @@ class About extends React.Component{
   render(){
     
     return(
+      
       <div>
+         <div>
+      <userContext.Consumer>
+      {({loggedUser})=>(
+        <h1>User:{loggedUser}</h1>
+      )}
+      </userContext.Consumer>
+        </div>
+        <div>
+      <userContext.Consumer>
+      {({loggeduser})=>(
+        <h1>{loggeduser}</h1>
+      )}
+      </userContext.Consumer>
+        </div>
         <h1>About Page</h1>
        <h2>This is about Page</h2>
        <h3>------------------------------------------</h3>
