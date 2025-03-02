9318617b5d0336fd667b4617ba5951f95e5d3d9d
Aswin Reji <aswinreji.004@gmail.com>
2025-03-02 01:19:42 +0000

    feat: new proxy url to overwrite cors issue

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -1,7 +1,7 @@
 
 
 export const MENU_API_BASE_URL=
-"https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550";
+"https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550";
 
 
  export const imageUrl = 
