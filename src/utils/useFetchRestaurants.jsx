4a7f7c4a7da064e9e8bb75b77f473a1865082e1b
Aswin Reji <aswinreji.004@gmail.com>
2025-03-02 00:27:37 +0000

    feat: new proxy url to overwrite cors issue

--- src/utils/useFetchRestaurants.jsx
+++ src/utils/useFetchRestaurants.jsx
@@ -20,11 +20,13 @@ const useFetchRestaurants =()=>{
 
     
     // const data = await fetch("https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
-    const data = await fetch("https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
+    // const data = await fetch("https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
+     const data = await fetch("https://thingproxy-760k.onrender.com/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
     console.log(data,'data in the fetch')
 
     
     const json = await data.json()
+    console.log(json,'json is here')
     const restaurants =(json?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     setList(restaurants)
     setFilteredList(restaurants)
